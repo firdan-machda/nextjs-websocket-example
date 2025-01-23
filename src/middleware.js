90364004d16f0f007c603adb472be57cff3fe9ea
firdan <onpeci@gmail.com>
2025-01-23 17:31:01 +0700

    update: auth logic

--- src/middleware.js
+++ src/middleware.js
@@ -9,22 +9,22 @@ export const config = {
 }
 
 export function middleware(req) {
-  if (process.env.VERCEL_ENV === 'preview'){
-    const basicAuth = req.headers.get('authorization')
-    const url = req.nextUrl
+  // if (process.env.VERCEL_ENV === 'preview'){
+  const basicAuth = req.headers.get('authorization')
+  const url = req.nextUrl
 
-    if (basicAuth) {
-      const authValue = basicAuth.split(' ')[1]
-      const [user, pwd] = atob(authValue, 'base64').split(':')
-  
-      if (user === process.env.BASIC_AUTH_USERNAME && pwd === process.env.BASIC_AUTH_PASSWORD) {
-        return NextResponse.next()
-      }
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue, 'base64').split(':')
+
+    if (user === process.env.BASIC_AUTH_USERNAME && pwd === process.env.BASIC_AUTH_PASSWORD) {
+      return NextResponse.next()
     }
-  
-    url.pathname='/api/devauth'
-    return NextResponse.rewrite(url)
-  }else {
-    return NextResponse.next()
   }
+
+  url.pathname='/api/devauth'
+  return NextResponse.rewrite(url)
+  // }else {
+  //   return NextResponse.next()
+  // }
 }
